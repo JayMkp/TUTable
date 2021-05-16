a42dfb22b6c8f4553274d9ca43843300792d2b1b
JayMkp <kagisomakopo85@gmail.com>
2021-05-16 13:29:07 -0700

    Initial commit

---
+++ about.php
@@ -0,0 +1,23 @@
+<h1><?php echo $title;?></h1>
+<p> Our University was formed in 2004 following the merger of three technikons. Our merger aimed at redressing
+the inequalities of the past by providing access to all our communities and an academic home for all our
+students.
+In that short time, we have grown into the largest contact University in South Africa, with seven faculties
+offering programmes in science, engineering and technology, business and management, education, humanities
+and the arts.
+We have been ranked by Times Higher Education (THE) in 2018 as one of the top 1000 universities globally and
+our programmes in engineering in the top 300. THE has also ranked us eighth in South Africa. In 2017, University
+Ranking by Academic Performance (URAP) ranked us as the number one University of Technology in Africa.
+Equally important, as a people’s university our moral purpose is to be the embodiment and custodian of our
+people’s aspirations. This strategic intent is foregrounded by the outcomes of our Transformation Summit held
+in 2017 and subsequent Transformation Framework, which was informed by national, continental and global
+developmental goals.
+Our University provides an academic home for all our people, especially for those who are at the margins of
+society, rural and urban, by maximising access to higher education to address our triple challenges of poverty,
+inequality and unemployment.
+We adopt a decolonial position as our programmes aim to have a liberating effect by ensuring that our
+learning becomes a continuum of creation, innovation and technology transfer to serve the aspirations of our
+communities.
+Anchored in the African philosophy of Ubuntu, we are a caring and compassionate University and a site of
+ideation, intellectualism and engaged scholarship to effect social change and transformation. 
+</p>
---
+++ home.php
@@ -0,0 +1,16 @@
+<h1><?php echo $title;?></h1>
+<div class="col-lg-6">
+	<h3>PDF</h3>
+	<div class="table-responsive">
+		<table id="example" class="table table-bordered">
+			<thead>
+				<th width="2%">Date</th>
+				<th>News Feed</th>
+			</thead>
+			<tbody>
+		
+		
+			</tbody>
+		</table>
+	</div>
+</div>
